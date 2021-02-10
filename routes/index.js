8764368840bebf511b3d83f037cfc21d8b815e5a
Kai Takeshima <takeshimakai@gmail.com>
2021-02-10 12:14:28 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date()
+  });
+  res.redirect('/')
+})
 
 module.exports = router;
